e5d74b647bede3355c0417d3b1887e1b81a6af87
Alb3rt03rik <erikalberto8492@gmail.com>
2021-01-15 21:32:51 -0400

    fixing bug for the fing method on numFinder class

--- src/main/java/tudelft/numfinder/NumFinder.java
+++ src/main/java/tudelft/numfinder/NumFinder.java
@@ -5,13 +5,17 @@ public class NumFinder {
     private int largest = Integer.MIN_VALUE;
 
     public void find(int[] nums) {
-        for(int n : nums) {
+        if(nums.length != 0){
+            for(int n : nums) {
 
-            if(n < smallest)
-                smallest = n;
-            else if (n > largest)
-                largest = n;
+                if(n < smallest)
+                    smallest = n;
+                if (n > largest)
+                    largest = n;
 
+            }
+        }else{
+            System.out.println("array is empty");
         }
     }
 
